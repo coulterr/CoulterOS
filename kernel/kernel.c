375783cd00a8c6ffc3815fc6cc0a1e4c31f64343
Riley <rcoulter35@gmail.com>
2016-11-20 17:24:42 -0500

    revised ordering driver initializations

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,30 +7,29 @@
 int init_kernel()
 {
 
+	init_vga(); 
+	init_ps2();
+
 	init_mallocator((void *) 0x5000); 
 	
 	char_queue *ps2_queue = (char_queue *) mallocate(sizeof(char_queue) + sizeof(char) * 20); 
 	char_queue_init(ps2_queue); 
 
-	
 	char_queue *vga_queue = (char_queue *) mallocate(sizeof(char_queue) + sizeof(char) * 20); 
 	char_queue_init(vga_queue); 
 
 
-	init_vga(); 
 	vga_set_queue(vga_queue); 
+	ps2_set_active_queue(ps2_queue); 
+	ps2_set_vga_queue(vga_queue); 
+	
 	carriage_return(); 
 	print_prompt(); 
 
-
-	init_ps2(); 
-	ps2_set_active_queue(ps2_queue); 
-	ps2_set_vga_queue(vga_queue); 
-
 	char_queue_enqueue(vga_queue, 'H'); 
 	print_char(); 
-	
 
+	
 	while(1)
 	{
 	}
